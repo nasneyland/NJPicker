8339c86563e4c47ab3200d4646da87f6dc85810e
신나진 <113892130+nasneyland@users.noreply.github.com>
2023-08-25 16:37:43 +0900

    project: create package

---
+++ Tests/NJPickerTests/NJPickerTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import NJPicker
+
+final class NJPickerTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(NJPicker().text, "Hello, World!")
+    }
+}
